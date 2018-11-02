71299528df6a09ee29e6cb59b01c25abc226c5a4
Stefan Froemken <froemken@gmail.com>
2018-11-02 10:46:12 +0100

    Update requirement of service_bw2 to 1.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF['masterplan'] = [
         'depends' => [
             'typo3' => '8.7.0-8.7.99',
             'maps2' => '4.1.0-4.99.99',
-            'service_bw2' => '0.0.1-1.99.99'
+            'service_bw2' => '1.4.0-1.99.99'
         ],
         'conflicts' => [],
         'suggests' => [],
