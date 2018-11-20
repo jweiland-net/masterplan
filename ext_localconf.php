a828126696bc0f4941287c60bac46b14332479f6
Stefan Froemken <froemken@gmail.com>
2018-11-20 17:02:03 +0100

    Move all TCA modifying methods into Overrides

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,17 +3,15 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-call_user_func(function($extKey) {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'JWeiland.' . $extKey,
-        'Masterplan',
-        [
-            'Project' => 'list, show',
-            'Location' => 'show'
-        ],
-        // non-cacheable actions
-        [
-            'Project' => '',
-        ]
-    );
-}, $_EXTKEY);
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'JWeiland.masterplan',
+    'Masterplan',
+    [
+        'Project' => 'list, show',
+        'Location' => 'show'
+    ],
+    // non-cacheable actions
+    [
+        'Project' => '',
+    ]
+);
--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,11 @@
 <?php
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'JWeiland.masterplan',
-	'Masterplan',
-	'Masterplan'
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_masterplan_domain_model_project',
+    'EXT:masterplan/Resources/Private/Language/locallang_csh_tx_masterplan_domain_model_project.xlf'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('masterplan', 'Configuration/TypoScript', 'Masterplan');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_masterplan_domain_model_project', 'EXT:masterplan/Resources/Private/Language/locallang_csh_tx_masterplan_domain_model_project.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_masterplan_domain_model_project');
