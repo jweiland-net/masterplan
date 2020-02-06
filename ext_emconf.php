70570c67f9c73234b8bf289699259a93fece7b76
Stefan Froemken <froemken@gmail.com>
2020-02-06 14:57:55 +0100

    Update to version 2.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,12 +19,12 @@ $EM_CONF['masterplan'] = [
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.1',
+    'version' => '2.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
             'maps2' => '7.1.3-7.99.99',
-            'service_bw2' => '2.0.0-2.99.99'
+            'service_bw2' => '3.0.0-3.99.99'
         ],
         'conflicts' => [],
         'suggests' => [],
