fd23d60ef74833c9176192088198087070c5cb1a
Stefan Froemken <froemken@gmail.com>
2020-12-07 11:12:57 +0100

    Use new Extension Configuration Service in ExtConf

--- Classes/Configuration/ExtConf.php
+++ Classes/Configuration/ExtConf.php
@@ -11,7 +11,9 @@ declare(strict_types = 1);
 
 namespace JWeiland\Masterplan\Configuration;
 
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * This class will streamline the values from extension manager configuration
@@ -25,35 +27,24 @@ class ExtConf implements SingletonInterface
 
     public function __construct()
     {
-        if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['masterplan'])) {
-            // get global configuration
-            $extConf = unserialize(
-                $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['masterplan'],
-                ['allowed_classes' => false]
-            );
-            if (is_array($extConf) && count($extConf)) {
-                // call setter method foreach configuration entry
-                foreach ($extConf as $key => $value) {
-                    $methodName = 'set' . ucfirst($key);
-                    if (method_exists($this, $methodName)) {
-                        $this->$methodName($value);
-                    }
+        // get global configuration
+        $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('masterplan');
+        if (is_array($extConf) && count($extConf)) {
+            // call setter method foreach configuration entry
+            foreach ($extConf as $key => $value) {
+                $methodName = 'set' . ucfirst($key);
+                if (method_exists($this, $methodName)) {
+                    $this->$methodName((string)$value);
                 }
             }
         }
     }
 
-    /**
-     * @return int
-     */
     public function getRootCategory(): int
     {
-        return (int)$this->rootCategory;
+        return $this->rootCategory;
     }
 
-    /**
-     * @param int $rootCategory
-     */
     public function setRootCategory($rootCategory)
     {
         $this->rootCategory = (int)$rootCategory;
