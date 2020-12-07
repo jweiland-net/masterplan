1f569bfe988aba02e7b6f3fdedbd4eae49c07a97
Stefan Froemken <froemken@gmail.com>
2020-12-07 12:30:53 +0100

    Use ExtensionConfiguration to get rootCategory

--- Configuration/TCA/Overrides/tx_masterplan_domain_model_project.php
+++ Configuration/TCA/Overrides/tx_masterplan_domain_model_project.php
@@ -1,5 +1,9 @@
 <?php
-call_user_func(function ($extConf) {
+call_user_func(function () {
+    $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+    );
+
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
         'masterplan',
         'tx_masterplan_domain_model_project',
@@ -8,7 +12,7 @@ call_user_func(function ($extConf) {
             'label' => 'LLL:EXT:masterplan/Resources/Private/Language/locallang_db.xlf:tx_masterplan_domain_model_project.area_of_activity',
             'fieldConfiguration' => [
                 'treeConfig' => [
-                    'rootUid' => $extConf['rootCategory']
+                    'rootUid' => $extensionConfiguration->get('masterplan', 'rootCategory')
                 ]
             ]
         ]
@@ -21,4 +25,4 @@ call_user_func(function ($extConf) {
             'tx_masterplan_domain_model_project'
         );
     }
-}, unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['masterplan']));
+});
