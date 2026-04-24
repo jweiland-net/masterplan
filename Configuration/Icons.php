224dea18468e702de951ba92c17b204210263de1
Stefan Froemken <sfroemken@jweiland.net>
2026-04-24 09:28:12 +0200

    [TASK] Format Icons.php for improved readability
    - Reformatted array structure for better clarity
    - Enhanced readability and consistency in icons configuration

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -10,5 +10,8 @@
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
 return [
-    'ext-masterplan-masterplan-wizard-icon' => ['provider' => SvgIconProvider::class, 'source' => 'EXT:masterplan/Resources/Public/Icons/plugin_wizard.svg'],
+    'ext-masterplan-masterplan-wizard-icon' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:masterplan/Resources/Public/Icons/plugin_wizard.svg',
+    ],
 ];
